c7dab6b76c5f714c73aee981bdb2601aa4028177
Shashank Kushwaha <kushwahashashank71@gmail.com>
2025-09-28 18:18:29 +0000

    container config done

---
+++ chaijs/4.js
@@ -0,0 +1,17 @@
+const accountid = 144556
+let accountEmail = "eskay@tutunota.in"
+var accoutPass = "445566"
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+accountcity = "Kanpur"
+//data can be stored in accountcity or any other variable wihtout being declared .
+
+//accountid = 2 // not allowed
+
+accountEmail = "milan@hdd.in"
+
+console.log(accountid);
+
+console.table([accountid , accountEmail , accoutPass , accountcity]);
